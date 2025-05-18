7ff18fa4c1e801b474a4f2f8089650d0207187de
AngeloSmidt <230688020@mycput.ac.za>
2025-05-18 16:50:22 +0200

    Session and University factory and factory test classes completed

---
+++ src/main/java/za/ac/cput/Factory/UniversityFactory.java
@@ -0,0 +1,21 @@
+package za.ac.cput.Factory;
+/*  UniversityFactory.java
+    University Factory class
+    Author: Angelo Smidt - 230688020
+    Date: 18 May 2025
+ */
+import za.ac.cput.domain.University;
+
+public class UniversityFactory {
+    public static University createUniversity(String universityName, String location, String domain){
+
+        //String universityId = Helper.generateId();
+
+        return new University.UniversityBuilder()
+                //.universityId(universityId)
+                .universityName(universityName)
+                .location(location)
+                .domain(domain)
+                .build();
+    }
+}
