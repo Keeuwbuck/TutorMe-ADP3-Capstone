87f826f711144071485d76989af809149906bd0f
AngeloSmidt <230688020@mycput.ac.za>
2025-05-18 23:48:12 +0200

    Changed variable types of startTime and endTime from int to LocalDateTime

--- src/test/java/za/ac/cput/factory/SessionFactoryTest.java
+++ src/test/java/za/ac/cput/factory/SessionFactoryTest.java
@@ -13,9 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SessionFactoryTest {
     @Test
     void createSession_Successful(){
-        Session session = SessionFactory.createSession(12, 13, "CPUT", "Online", 25, "Under way", "None");
-        assertEquals(12, session.getStartTime());
-        assertEquals(13, session.getEndTime());
+        LocalDateTime startTime1 = LocalDateTime.of(2025, 5, 18, 12, 0);
+        LocalDateTime endTime1 = LocalDateTime.of(2025, 5, 18, 13, 0);
+        Session session = SessionFactory.createSession(startTime1, endTime1, "CPUT", "Online", 25, "Under way", "None");
+        assertEquals(startTime1, session.getStartTime());
+        assertEquals(endTime1, session.getEndTime());
         assertEquals("CPUT", session.getLocation());
         assertEquals("Online", session.getMode());
         assertEquals(25, session.getCost());
