c8d923b021bd58587ea012265f604d02f058710e
AngeloSmidt <230688020@mycput.ac.za>
2025-05-25 21:10:17 +0200

    Updated all SessionService classes, UniversityService classes, UniversityRepository and SessionRepository

--- src/main/java/za/ac/cput/service/IUniversityService.java
+++ src/main/java/za/ac/cput/service/IUniversityService.java
@@ -1,4 +1,8 @@
 package za.ac.cput.service;
-
+/*  IUniversityService.java
+    University Service interface
+    Author: Angelo Smidt - 230688020
+    Date: 25 May 2025
+ */
 public interface IUniversityService {
 }
