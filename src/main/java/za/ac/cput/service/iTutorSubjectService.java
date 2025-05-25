9e18de9c0418c4f7bc2f0750a5aa46c2800598f7
keewan10 <keewantitus01@gmail.com>
2025-05-25 23:20:42 +0200

    Create SubjectFactory, TutorSubjectFactory and SubjectFactoryTest and TutorSubjectFactoryTest

---
+++ src/main/java/za/ac/cput/service/iTutorSubjectService.java
@@ -0,0 +1,13 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Subject;
+import za.ac.cput.domain.TutorSubject;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface iTutorSubjectService {
+    List<Subject> getAll();
+    Optional<Subject> getSubjectId(String subjectId);
+
+}
