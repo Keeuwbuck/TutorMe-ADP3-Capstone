450906c4939e29cd04380d5b84d0ef86683940d6
Hope Ringane <hopenthembo20@gmail.com>
2025-05-25 22:48:46 +0200

    factory for User and Availability

---
+++ src/main/java/za/ac/cput/service/IUserService.java
@@ -0,0 +1,12 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.User;
+import java.util.List;
+
+public interface IUserService {
+    User create(User user);
+    User read(String id);
+    User update(User user);
+    boolean delete(String id);
+    List<User> getAll();
+}
