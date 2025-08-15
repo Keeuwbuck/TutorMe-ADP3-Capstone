2f565d94db5a01df3b36da4807653acbf0d02c1d
Henzley <spoggie.hs@gmail.com>
2025-08-15 20:07:34 +0200

    Annotate Main class properly to indicate it is a Spring Boot application.

--- spring-boot-application/src/main/java/za/ac/cput/Main.java
+++ spring-boot-application/src/main/java/za/ac/cput/Main.java
@@ -1,7 +1,12 @@
 package za.ac.cput;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import za.ac.cput.domain.Tutor;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
