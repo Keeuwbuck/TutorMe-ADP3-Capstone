c86b8d64a270daea372a158ce4e9d8b8f9d9a323
Henzley <140068322+Henzley@users.noreply.github.com>
2025-08-15 14:41:12 +0200

    Delete src directory
    Directory is redundant.
    Found in 'spring-boot-application' folder

--- src/main/java/za/ac/cput/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cput.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.domain.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-}
