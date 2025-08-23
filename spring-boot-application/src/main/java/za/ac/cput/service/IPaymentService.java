5b4cce301258f519122eb1c753569cf09bf65c22
Henzley <spoggie.hs@gmail.com>
2025-08-23 19:35:26 +0200

    Add CRUD & getAll methods.

--- spring-boot-application/src/main/java/za/ac/cput/service/IPaymentService.java
+++ spring-boot-application/src/main/java/za/ac/cput/service/IPaymentService.java
@@ -1,4 +1,17 @@
 package za.ac.cput.service;
 
+import za.ac.cput.domain.Payment;
+
+import java.util.List;
+
 public interface IPaymentService {
+    Payment create(Payment payment);
+
+    Payment read(String paymentID);
+
+    Payment update(Payment payment);
+
+    boolean delete(String paymentID);
+
+    List<Payment> getAll();
 }
