62167c35c51da22cddf7300740d738df3a0ab56f
Henzley <spoggie.hs@gmail.com>
2025-08-23 19:36:08 +0200

    Create implementation classes.

--- spring-boot-application/src/main/java/za/ac/cput/service/PaymentService.java
+++ spring-boot-application/src/main/java/za/ac/cput/service/PaymentService.java
@@ -1,4 +1,43 @@
 package za.ac.cput.service;
 
-public class PaymentService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Payment;
+import za.ac.cput.repository.PaymentRepository;
+
+import java.util.List;
+
+@Service
+public class PaymentService implements IPaymentService {
+
+    @Autowired
+    private static PaymentService service;
+
+    private PaymentRepository repository;
+
+    @Override
+    public Payment create(Payment payment) {
+        return this.repository.save(payment);
+    }
+
+    @Override
+    public Payment read(String paymentID) {
+        return this.repository.findById(paymentID).orElse(null);
+    }
+
+    @Override
+    public Payment update(Payment payment) {
+        return this.repository.save(payment);
+    }
+
+    @Override
+    public boolean delete(String paymentID) {
+        this.repository.deleteById(paymentID);
+        return true;
+    }
+
+    @Override
+    public List<Payment> getAll() {
+        return this.repository.findAll();
+    }
 }
--- spring-boot-application/src/main/java/za/ac/cput/service/ReviewService.java
+++ spring-boot-application/src/main/java/za/ac/cput/service/ReviewService.java
@@ -1,4 +1,42 @@
 package za.ac.cput.service;
 
-public class ReviewService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Review;
+import za.ac.cput.repository.ReviewRepository;
+
+import java.util.List;
+
+@Service
+public class ReviewService implements IReviewService {
+    @Autowired
+    private static ReviewService service;
+
+    private ReviewRepository repository;
+
+    @Override
+    public Review create(Review review) {
+        return this.repository.save(review);
+    }
+
+    @Override
+    public Review read(String reviewID) {
+        return this.repository.findById(reviewID).orElse(null);
+    }
+
+    @Override
+    public Review update(Review review) {
+        return this.repository.save(review);
+    }
+
+    @Override
+    public boolean delete(String reviewID) {
+        this.repository.deleteById(reviewID);
+        return true;
+    }
+
+    @Override
+    public List<Review> getAll() {
+        return this.repository.findAll();
+    }
 }
